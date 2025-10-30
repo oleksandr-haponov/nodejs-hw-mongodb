8dffb7c82b6369d08962aa8821a48e47db47321e
Oleksandr Haponov <olex.haponov@gmail.com>
2025-10-30 11:00:46 +0200

    redirect

--- src/server.js
+++ src/server.js
@@ -13,12 +13,12 @@ export const setupServer = () => {
   app.use(express.json());
   app.use(pinoHttp({ logger }));
 
-  //* temp
-  app.get('/', (req, res) => {
-    res.redirect(307, '/api');
-  });
-  app.get('/contacts', (req, res) => {
-    res.redirect(307, '/api/contacts');
+  app.use((req, res, next) => {
+    if (!req.path.startsWith('/api')) {
+      req.log.info(`Redirecting ${req.originalUrl} -> /api${req.originalUrl}`);
+      return res.redirect(307, `/api${req.originalUrl}`);
+    }
+    next();
   });
 
   app.get('/api', (req, res) => {
